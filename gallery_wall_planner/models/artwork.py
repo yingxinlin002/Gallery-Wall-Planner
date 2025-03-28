c362590124488b45e6dac722c968509cf6e68d74
zenithpheonix <85771069+zenithpheonix@users.noreply.github.com>
2025-03-28 07:50:07 -0500

    Update artwork.py attributes
    add notes attribute to be consistent with excel sheet

--- gallery_wall_planner/models/artwork.py
+++ gallery_wall_planner/models/artwork.py
@@ -2,7 +2,7 @@ import json
 from types import SimpleNamespace
 
 class Artwork:
-    def __init__(self, name = "", medium = "", height = 0, width = 0, depth = 0, hanging_point = 0, price = 0, nfs = False, image_path = ""):
+    def __init__(self, name = "", medium = "", height = 0, width = 0, depth = 0, hanging_point = 0, price = 0, nfs = False, image_path = "", notes = ""):
         self.name = name
         self.medium = medium
         self.height = height
@@ -12,6 +12,7 @@ class Artwork:
         self.price = float(price)
         self.nfs = nfs
         self.image_path = image_path
+        self.notes = notes
 
     def __str__(self):
         #return all info about the artwork
