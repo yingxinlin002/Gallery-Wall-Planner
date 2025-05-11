0d893b4a83edba53e2e70d7b21513e6956c53af7
Hunter Ward <hdoubleya@yahoo.com>
2025-05-10 22:38:09 -0500

    Add user directory creation for saving files

--- gallery_wall_planner/gui/app_main.py
+++ gallery_wall_planner/gui/app_main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from enum import Enum, auto
 from typing import Optional
+import os
 
 from gallery_wall_planner.models.gallery import Gallery
 from gallery_wall_planner.models.artwork import Artwork  # Import Artwork class
@@ -19,11 +20,14 @@ class ScreenType(Enum):
 
 class AppMain():
     def __init__(self, root: tk.Tk):
+        self._create_user_dir()
+
         self.gallery = Gallery()
         print("Creating main application window...")
         self.root = root
         self.root.title("Gallery Wall Planner")
         self.root.geometry("1024x768")
+        self.root.protocol("WM_DELETE_WINDOW", self.quit_application)
         self.root.configure(bg="#F0F0F0")
         print("Creating main frame...")
 
@@ -121,6 +125,22 @@ class AppMain():
         from gallery_wall_planner.gui.screen_artwork_manually_ui import ScreenArtworkManuallyUI
         self.frame_contents = ScreenArtworkManuallyUI(self)
 
+    def _create_user_dir(self):
+        """Create a user directory for saving files."""
+
+
+        if os.name == 'nt':
+            home_dir = os.path.expanduser("~")
+
+            user_dir = os.path.join(home_dir, "GalleryWallPlanner")
+            print(user_dir)
+            if not os.path.exists(user_dir):
+                os.makedirs(user_dir)
+                print(f"User directory created at: {user_dir}")
+            else:
+                print(f"User directory already exists at: {user_dir}")
+
     def quit_application(self):
         """Quit the application."""
+        print("Quitting application...")
         self.root.destroy()
